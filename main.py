8bfc0a486d5f0768d0a96e0aca29e540061efb9d
ck-code-98 <constantin-knappe@web.de>
2025-08-26 19:42:57 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,53 @@
+# github
+
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+class Store:
+    def __init__(self):
+        print("Store has been created")
+
+
+
+"""
+ Store Menu
+   ----------
+1. List all products in store
+2. Show total amount in store
+3. Make an order
+4. Quit
+Please choose a number: 1
+------
+1. MacBook Air M2, Price: $1450, Quantity: 100
+2. Bose QuietComfort Earbuds, Price: $250, Quantity: 500
+3. Google Pixel 7, Price: $500, Quantity: 250
+------
+
+   Store Menu
+   ----------
+1. List all products in store
+2. Show total amount in store
+3. Make an order
+4. Quit
+Please choose a number: 2
+Total of 850 items in store
+
+   Store Menu
+   ----------
+1. List all products in store
+2. Show total amount in store
+3. Make an order
+4. Quit
+Please choose a number: 3
+------
+1. MacBook Air M2, Price: $1450, Quantity: 100
+2. Bose QuietComfort Earbuds, Price: $250, Quantity: 500
+3. Google Pixel 7, Price: $500, Quantity: 250
+------
+When you want to finish order, enter empty text.
+Which product # do you want? 1
+What amount do you want? 1
+Product added to list!
+"""
